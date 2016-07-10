af717b4506489b89af63fd38c4c4ad5e404d54ba
icrustandi <icrustandi@gmail.com>
2016-07-10 05:50:12 +0000

    Added modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,7 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
+
 
   def create
     @quote=Quote.create(quote_params)
@@ -22,6 +20,6 @@ class QuotesController < ApplicationController
 
   def about
   end
-  
+
 
 end
